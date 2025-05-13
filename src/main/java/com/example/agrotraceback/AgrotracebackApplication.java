0f0f741dea99cef71bd5ca15e28b45e806c1152b
JoaoALT <joaomuob@unisabana.edu.co>
2025-05-13 11:02:57 -0500

    Initial springboot configuration of back

---
+++ src/main/java/com/example/agrotraceback/AgrotracebackApplication.java
@@ -0,0 +1,13 @@
+package com.example.agrotraceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrotracebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrotracebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agrotraceback/AgrotracebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agrotraceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrotracebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
